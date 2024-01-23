8ce475c29bb9a3c10789d6fbbd1a5a5891a077ee
Mitchell van Rijswijk <mitchell.van.rijswijk@gmail.com>
2024-01-23 20:00:56 +0100

    Add Firebase key constants

---
+++ src/main/java/com/harbour/clearview/api/datasource/util/FirebaseConstants.java
@@ -0,0 +1,11 @@
+package com.harbour.clearview.api.datasource.util;
+
+public class FirebaseConstants {
+    public static final String TODO_LISTS_COLLECTION_NAME = "todoLists";
+    public static final String TODO_LIST_DATE_KEY = "date";
+    public static final String TODO_LIST_TITLE_KEY = "title";
+    public static final String TODO_LIST_TODOS_KEY = "todos";
+    public static final String TODO_TITLE_KEY = "title";
+    public static final String TODO_DESCRIPTION_KEY = "description";
+    public static final String TODO_COMPLETED_KEY = "isCompleted";
+}
